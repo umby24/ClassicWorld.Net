c96e8b687b60895ef085b4c2876ab72bc1948450
umby24 <wanderingscripter@gmail.com>
2014-01-19 14:07:27 -0600

    Added graphical Unit Test program
    Tests loading of maps, saving of maps, and creating + saving maps.

---
+++ ClassicWorldTest/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ClassicWorldTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.openMap = new System.Windows.Forms.OpenFileDialog();
+            this.saveMap = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load File...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 41);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save File...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(205, 57);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(33, 13);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Time:";
+            // 
+            // openMap
+            // 
+            this.openMap.DefaultExt = "cw";
+            this.openMap.Filter = "ClassicWorld Map|*.cw";
+            // 
+            // saveMap
+            // 
+            this.saveMap.DefaultExt = "cw";
+            this.saveMap.Filter = "ClassicWorld Map|*.cw";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 79);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "ClassicWorldTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.OpenFileDialog openMap;
+        private System.Windows.Forms.SaveFileDialog saveMap;
+    }
+}
+
---
+++ ClassicWorldTest/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using ClassicWorld_NET;
+
+namespace ClassicWorldTest {
+    public partial class Form1 : Form {
+        bool Loaded = false;
+        ClassicWorld Map;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e) {
+            var Result = openMap.ShowDialog();
+
+            if (Result == DialogResult.OK) {
+                Stopwatch Timer = new Stopwatch();
+                Timer.Start();
+
+                Map = new ClassicWorld(openMap.FileName);
+                Map.Load();
+
+                Timer.Stop();
+                lblTime.Text = "Time: " + Timer.Elapsed.TotalSeconds.ToString();
+                Loaded = true;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            var Result = saveMap.ShowDialog();
+
+            if (Result == DialogResult.OK) { 
+                if (Loaded) {
+                    Stopwatch Timer = new Stopwatch();
+                    Timer.Start();
+
+                    Map.Save(saveMap.FileName);
+
+                    Timer.Stop();
+
+                    lblTime.Text = "Time: " + Timer.Elapsed.TotalSeconds.ToString();
+                } else {
+                    // -- Create a map and save it.
+                    Stopwatch Timer = new Stopwatch();
+                    Timer.Start();
+
+                    Map = new ClassicWorld(128, 128, 128);
+                    Map.MapName = "New Map!";
+                    Map.Save(saveMap.FileName);
+
+                    Timer.Stop();
+
+                    lblTime.Text = "Time: " + Timer.Elapsed.TotalSeconds.ToString();
+                }
+            }
+        }
+    }
+}
